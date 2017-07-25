4076a7dd79e508dc8f3fd4fe6f71a3dc7612f19e
Mykhailo Hodis <godismichael1995@gmail.com>
2017-07-26 00:41:33 +0300

    - Initial commit

---
+++ soup.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import requests, re
+import pandas as pd
+
+page = 'http://www.bbc.com/news'
+open_page = requests.get(page).text
+soup = BeautifulSoup(open_page, 'lxml')
+
+recent_news = soup.find('div', attrs={'class': 'gel-wrap gs-u-pt+'})
+
+articles = recent_news(['h3', 'time'])
+time_pattern = r'^[0-9]{1,2}[h,m]{1}'
+
+print('Most recent news:')
+for article in articles:
+    print(re.sub(time_pattern, '', article.text))
